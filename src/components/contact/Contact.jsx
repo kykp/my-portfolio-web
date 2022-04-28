e4123c637554e1197af06efb97f428f4864472c1
kykp <kykp@yandex.ru>
2022-04-28 11:33:39 +0400

    form button disabled and popup form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useState, useEffect } from "react"
 import "./contact.css"
 import {MdOutlineEmail} from "react-icons/md"
 import {RiMessengerLine} from "react-icons/ri"
 import { useRef } from 'react'
 import emailjs from "emailjs-com"
 
+
 const Contact = () => {
+  const [active, setActive] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [buttonActive, setButtonActive] = useState(false);
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -20,14 +28,37 @@ const Contact = () => {
       e.target.reset();
   };
 
+  useEffect(() => {
+    if (active === true) {
+      setTimeout(() => {
+        setActive(false);
+      }, 2000);
+    } 
+  },[active])
+
+  useEffect(() => {
+   if (name.length > 0 && email.length > 0 && message.length > 0) {
+     setButtonActive(true);
+   } else {
+     setButtonActive(false);
+   }
+  },[name, email, message])
+
+  const onHandleSubmit = () => {
+      setActive(true);
+  }
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
       <div className='container contact__container'>
+      
         <div className="contact__options">
+          
           <article className="contact__option">
+            
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>kykp@yandex.ru</h5>
@@ -43,12 +74,21 @@ const Contact = () => {
         {/* END OF CONTACT OPTIONS */}
 
       <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name="name" placeholder='Your Full Name'  required/>
-        <input type="email" name="email" placeholder='Yuor Email' required />
-        <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-        <button type='sumbit' className='btn btn-primary'>Send Message</button>
+        
+        <input onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder='Your Full Name'  required/>
+        <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder='Yuor Email' required />
+        <textarea onChange={(e) => setMessage(e.target.value)} name="message" rows="7" placeholder='Your Message' required></textarea>
+        <button disabled={buttonActive? null : "disabled"} onClick={onHandleSubmit} type='sumbit' className='btn btn-primary'>
+          {buttonActive? "Send Message" : "Filled Up Form Please"}</button>
+        {/* MODALE WINDOW START */}
+        <div className={active? "modale_active":"modale"}>
+          <h2>Message was sent</h2>
+        </div>
+        {/* MODALE WINDOW END */}
       </form>
+      
       </div>
+      
     </section>
   )
 }
