2e3c0e08223a8de2131f9ca6c818855378afbe14
kykp <kykp@yandex.ru>
2022-05-04 16:46:11 +0400

    added css card new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,44 +6,53 @@ import IMG3 from "../../assets/portfolio3.jpg"
 import IMG4 from "../../assets/portfolio4.jpg"
 import IMG5 from "../../assets/portfolio5.jpg"
 import IMG6 from "../../assets/portfolio6.jpg"
+import IMG7 from "../../assets/portfolio7.jpg"
 import {useOnScreen} from "../hooks/UseOnScreen"
 
-const data = [{
-  id: 1,
-  image: IMG3,
+const data = [
+  {
+    id: 7,
+    image: IMG7,
+    title: "Expanding Cards",
+    github: "https://github.com/kykp/css_gallery",
+    demo: "https://css-gallery-virid.vercel.app/"
+  },
+  {
+  id: 6,
+  image: IMG6,
   title: "Mesto",
   github: "https://github.com/kykp/mesto",
   demo: "https://kykp.github.io/mesto/"
 },
 {
-  id: 2,
-  image: IMG2,
+  id: 5,
+  image: IMG5,
   title: "Russian Travel",
   github: "https://github.com/kykp/russian-travel",
   demo: "https://kykp.github.io/russian-travel/index.html"
 },
 {
-  id: 3,
-  image: IMG1,
+  id: 4,
+  image: IMG4,
   title: " Video-Catalog",
   github: "https://github.com/kykp/React-Video-Catalog",
   demo: "https://kykp.github.io/React-Video-Catalog/"
 },
 {
-  id: 4,
-  image: IMG4,
+  id: 3,
+  image: IMG3,
   title: "ToDo list on React",
   github: "https://github.com/kykp/ReactLearn1.io",
   demo: "https://kykp.github.io/ReactLearn1.io/"
 }, {
-  id:5,
-  image: IMG5,
+  id:2,
+  image: IMG2,
   title: "First SPA React",
   github: "https://github.com/kykp/spa-react",
   demo: "https://spa-react-ka5j77d00-kykp.vercel.app/"
 }, {
-  id:6,
-  image: IMG6,
+  id:1,
+  image: IMG1,
   title: "E-commerce",
   github: "https://github.com/kykp/e-shop",
   demo: "https://kykp.github.io/e-shop/"
