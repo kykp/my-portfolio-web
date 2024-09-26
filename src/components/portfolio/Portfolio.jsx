61762101471143eacb90013907ca9432b7c98d32
Konstantin Rev <kykp@yandex.ru>
2024-09-26 10:34:20 +0400

    fix: фото для проекта пофикшено

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -26,7 +26,7 @@ import { useOnScreen } from "../hooks/UseOnScreen";
 const data = [
   {
     id: 21,
-    image: '',
+    image: 'https://media.istockphoto.com/id/1440483244/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D1%81%D1%82%D0%B0%D0%B4%D0%B8%D0%BE%D0%BD-%D1%86%D0%B5%D0%BD%D1%82%D1%80-%D0%BC%D1%8F%D1%87%D0%B0-%D0%BF%D0%BE%D0%BB%D1%83%D0%B7%D0%B0%D1%89%D0%B8%D1%82%D0%B0.jpg?s=612x612&w=0&k=20&c=Hv8HU17bsgFv0mk2v7ltAN0qsBDoKI4ELbTGtMRiL44=',
     title: 'beGOAT.ru',
     demo: 'https://begoat.ru'
   },
