766257917985217477ef41ce3240135ae08e2e33
Konstantin Rev <kykp@yandex.ru>
2024-12-17 18:55:16 +0400

    new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -17,10 +17,17 @@ import IMG15 from "../../assets/portfolio15.jpg";
 import IMG16 from "../../assets/portfolio16.jpg";
 import IMG19 from "../../assets/portfolio19.png";
 import IMG20 from "../../assets/portfolio20.jpg";
+import IMG21 from "../../assets/portfolio22.jpg";
 
 import {useOnScreen} from "../hooks/UseOnScreen";
 
 const data = [
+  {
+    id: 22,
+    image: IMG21,
+    title: 'Life for Rent service',
+    demo: 'https://rent-for-life-3dua.vercel.app/?__vercel_draft=1'
+  },
   {
     id: 21,
     image: 'https://media.istockphoto.com/id/1440483244/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D1%81%D1%82%D0%B0%D0%B4%D0%B8%D0%BE%D0%BD-%D1%86%D0%B5%D0%BD%D1%82%D1%80-%D0%BC%D1%8F%D1%87%D0%B0-%D0%BF%D0%BE%D0%BB%D1%83%D0%B7%D0%B0%D1%89%D0%B8%D1%82%D0%B0.jpg?s=612x612&w=0&k=20&c=Hv8HU17bsgFv0mk2v7ltAN0qsBDoKI4ELbTGtMRiL44=',
